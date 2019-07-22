8aeba88e75f8fe344f6c3b1805fadfa8b651fc09
sanichyut <44532555+sanichyut@users.noreply.github.com>
2019-07-22 13:42:43 +0300

    Update statusbot.js

--- Discord-StatusBot-by sanich/statusbot.js
+++ Discord-StatusBot-by sanich/statusbot.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 var config = require('./config.json')
 let token = config.token
-let useridd = config.userid
 
 client.on("ready", () => {
     client.login(config.token);
@@ -10,17 +9,17 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-    if(message.author.id!=message.author.id) return;
+    if(message.author.id!=client.user.id) return;
     if (message.author.bot) return;
     if (message.channel.type == "dm") return;
     if (message.content == 'стрим'){
-        client.user.setPresence({ game: { name: `игрульки`, type: 'STREAMING', url: "https://www.twitch.tv/sanichof" } })
+        client.user.setPresence({ game: { name: `ксго`, type: 'STREAMING', url: "https://www.twitch.tv/sanichof" } })
         console.log(`У ${client.user.username} был сменён статус на "Стримит"`)};
     if (message.content == 'играть'){
-        client.user.setPresence({ game: { name: `дискорд`, type: 'PLAYING'} })   
+        client.user.setPresence({ game: { name: `тетрис`, type: 'PLAYING'} })   
         console.log(`У ${client.user.username} был сменён статус на "Играет"`)};
         if (message.content == 'слушать'){
-            client.user.setPresence({ game: { name: `музыку`, type: 'LISTENING'} })  
+            client.user.setPresence({ game: { name: `красивую музыку`, type: 'LISTENING'} })  
             console.log(`У ${client.user.username} был сменён статус на "Слушает"`)};  
             if (message.content == 'смотреть'){ 
                 client.user.setPresence({ game: { name: `мультики`, type: 'WATCHING'} })
